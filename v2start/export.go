b602f6f6bf1f8890736ce6d4188f801e0a7eded0
zhangmj <zhangmj1@dustess.com>
2024-12-09 17:43:32 +0800

    feat: v5 testing and export

--- v2start/export.go
+++ v2start/export.go
@@ -2,36 +2,10 @@ package v2start
 
 import (
 	core "github.com/make-money-fast/v2ray-core-v5"
-	"github.com/make-money-fast/v2ray-core-v5/main/commands/base"
 	_ "github.com/make-money-fast/v2ray-core-v5/main/distro/all"
-	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 )
 
-func Start(uri string) error {
-	server, err := startV2Ray(uri)
-	if err != nil {
-		return err
-	}
-	if err := server.Start(); err != nil {
-		base.Fatalf("Failed to start: %s", err)
-	}
-	defer server.Close()
-
-	// Explicitly triggering GC to remove garbage from config loading.
-	runtime.GC()
-
-	{
-		osSignals := make(chan os.Signal, 1)
-		signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
-		<-osSignals
-	}
-	return nil
-}
-
-func startV2Ray(uri string) (core.Server, error) {
+func Start(uri string) (core.Server, error) {
 	config, err := core.LoadConfig("json", uri)
 	if err != nil {
 		return nil, err
